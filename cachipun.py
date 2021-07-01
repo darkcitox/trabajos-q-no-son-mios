e30ac71ae6a12be1fe0df5a7f4142f2b305613d8
darkcitox <69071052+darkcitox@users.noreply.github.com>
2021-07-01 05:12:59 -0400

    Update cachipun.py

--- cachipun.py
+++ cachipun.py
@@ -1,6 +1,6 @@
 import random
 
-def play():
+def jugar():
     yo = input("piedra, papel o tijera\n")
     yo = yo.lower()
 
@@ -9,15 +9,15 @@ def play():
     if yo == pc:
         return "empate, elijieron {}".format(yo, pc)
 
-    if is_win(yo, pc):
+    if ganar(yo, pc):
         return "ganaste, {} le gana a {}".format(yo, pc)
 
-    return "perdiste, {} le gana a {}".format(yo, pc)
+    return "perdiste, {} no le gana a {}".format(yo, pc)
 
-def is_win(jugador, oponente):
+def ganar(jugador, oponente):
     if (jugador == 'piedra' and oponente == 'tijera') or (jugador == 'tijera' and oponente == 'papel') or (jugador == 'papel' and oponente == 'piedra'):
         return True
     return False
 
 if __name__ == '__main__':
-    print(play())
+    print(jugar())
