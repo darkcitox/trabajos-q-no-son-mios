b065ece7ddbfbe7911ccd8cdf412d1075cf586e3
darkcitox <69071052+darkcitox@users.noreply.github.com>
2021-07-02 15:24:54 -0400

    Update numero mayor.py

--- numero mayor.py
+++ numero mayor.py
@@ -3,10 +3,10 @@ num2 = int(input("Escribe el segundo número"))
 num3 = int(input("Escribe el tercer número"))
 
 if (num1 >= num2) and (num1 >= num3):
-   nromayor = num1
+   nummayor = num1
 elif (num2 >= num1) and (num2 >= num3):
-   nromayor = num2
+   nummayor = num2
 else:
-   nromayor = num3
+   nummayor = num3
 
-print("El número más grande es", nromayor)
+print("El número más grande es", nummayor)
